c64045d63f3bff8a3eb0da94c63b8f72e5dd0f8c
Gerald Hernandez <gerald2557@gmail.com>
2016-12-19 04:17:29 +0000

    created the dashbaord page for logged in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   def home
+    if current_user
+      redirect_to dashboard_path
+    end
+  end
+
+  def dashboard
+    @user = current_user
+    @reviews = current_user.reviews
+    @places = current_user.places
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :reviews
   devise_for :users, controllers: { registrations: 'registrations'}
   get 'pages/home'
-
+  get 'dashboard', to: 'pages#dashboard'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
