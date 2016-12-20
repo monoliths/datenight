d22cd9f43442488c244fb073151073c32e5deb0f
Gerald Hernandez <gerald2557@gmail.com>
2016-12-20 06:20:08 +0000

    added basic search

--- app/models/place.rb
+++ app/models/place.rb
@@ -17,10 +17,19 @@ class Place < ApplicationRecord
 	geocoded_by :address
 	after_validation :geocode
 
-	# function an instance can call to find the average rating
+	# method an instance can call to find the average rating
 	def average_rating
 		self.reviews.sum(:score) / reviews.size
 	rescue ZeroDivisionError
 		0
+	end
+
+	# method for making a text search for places
+	def self.search(term)
+		if term
+			where( [ 'name LIKE ? OR address LIKE ?', "%#{term}%", "#{term}%" ] )
+		else
+		  all
+		end
 	end
 end
